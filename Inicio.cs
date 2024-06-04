f4ea25ef031ed056c07a7f7b07055b40bc24255e
Farid Fragozo <faridfragozo@gmail.com>
2024-06-03 21:41:47 -0300

    Primera version

---
+++ Inicio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reparar
+{
+    class Inicio
+    {
+        public static void Main()
+        {
+            ArrayList ListaPro = new ArrayList();
+
+            Profesionales Profesional = new Profesionales("Arquitecto", 212312, "Colegio Estatal Ignacio San Justo", 500000, "Jorge", "Mata", 545577);
+
+
+
+            ListaPro.Add(Profesional);
+           
+            MostrarDatos(ListaPro);
+        }
+
+        public static void MostrarDatos(ArrayList profesionales)
+        {
+            foreach(var item in profesionales)
+            {
+                Profesionales profe = (Profesionales)item;
+
+                Console.WriteLine("Titulo: " + profe.TituloHab);
+                Console.WriteLine("Matricula: " + profe.matricula);
+                Console.WriteLine("Consejo Profesional: " + profe.consejo);
+                Console.WriteLine("Sueldo: " + profe.sueldo);
+                Console.WriteLine("Nombre: " + profe.nombre);
+                Console.WriteLine("Apellido: " + profe.apellido);
+                Console.WriteLine("Lgeajo: " + profe.legajo);
+            }
+        }
+    }
+}
---
+++ Obra.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reparar
+{
+    class Obra
+    {
+        private uint Codigo;
+        private string Direccion;
+        private string Matricula_Profesional;
+        private ArrayList Obreros = new ArrayList();
+
+        public Obra(uint cod, string dire, string mat_pro) 
+        {
+            Codigo = cod;
+            Direccion = dire;
+            Matricula_Profesional = mat_pro;
+        }
+
+        public uint codigo
+        {
+            get { return Codigo; }
+            set { Codigo = value; }
+        }
+        public string direccion
+        {
+            get { return Direccion; }
+            set { Direccion = value; }
+        }
+        public string matricula
+        {
+            get { return Matricula_Profesional; }
+            set { Matricula_Profesional = value;  }
+        }
+    }
+}
